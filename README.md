4e8403e85ee9869ecfc257fe7df145b7e428d808
= <=>
2021-08-26 23:29:17 +0200

    20210826: V1.0 Program is working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Barcodescanner
 
+The barcodescanner is working automatic once activated. start the programm with the command "python app.py" a new folder with the name "input" will apear, in which you can put a pdf that has a barcode.
+The Programm will split the pdf on each barcode to create new splitted PDF's. This can be used for invoices that have been marked with barcodes to be scanned with a scanner.
+Atm Warnings can happen from zbar, these can be ignored for now.
--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-import time
+import shutil
 import glob
+import asyncio
 import os
 import cv2
-import PyPDF2
+import shutup
+import warnings
 from os.path import join, basename
 from pdf2image import convert_from_path
 from pyzbar.pyzbar import decode
 from PyPDF2 import PdfFileWriter, PdfFileReader
+from datetime import date
 from re import compile
 
+
+# supress warnings
+shutup.please()
+
+
 #declaration
-batch_folder = './pdf/'
+batch_folder = './input/'
 image_folder = './img/'
+output_folder = './output/'
+archive_folder = './archiv/'
 
 #body
-def create_page_image():
+async def create_page_image():
     try:
         # Create target Directory
         os.mkdir('img')
+        os.mkdir('output')
     except FileExistsError:
         pass
     
-    for Pdf_file in glob.glob(join(batch_folder, '*.pdf')):
-        filename = basename(Pdf_file)
-        input_pdf = PdfFileReader('./pdf/' + filename)
+    for pdf_file in glob.glob(join(batch_folder, '*.pdf')):
+        filename = basename(pdf_file)
+        print('PDF found, processing: ' + filename)
+        input_pdf = PdfFileReader(batch_folder + filename)
         pagenum = 0
         pages = convert_from_path(batch_folder + filename, 500)
         
@@ -32,46 +44,83 @@ def create_page_image():
             pagenum += 1
             page.save(pagename, 'PNG')
             
-        check_code(input_pdf, filename)
+        await check_code(input_pdf, filename)
+        print('Finished processing: ' + filename)
+    return True
            
 
-def check_code(input_pdf, filename):
+async def check_code(input_pdf, filename):
     pagenum = 0
-    found  = False
-    output = PdfFileWriter()
+    pdfnum = 0
+    output = ""
     pdf_name = str(basename(filename)).replace('.pdf', '')
+    today = date.today()
+    current_date = today.strftime("%d/%m/%Y")
+    current_date = current_date.split('/')
+    day = current_date[0]
+    month = current_date[1]
+    year = current_date[2]
+
     for image in glob.glob(join(image_folder, '*.png')):
         read_image = cv2.imread(image)
         regex = compile(r'%s_(\d+)\.png' % (pdf_name))
+        warnings.filterwarnings('ignore')
         decode_image = decode(read_image)
-        pdf = PdfFileReader(open('./pdf/' + filename,'rb'))
-        pages = pdf.getNumPages()
-        if(int(pagenum) <= pages):
-            if decode_image: #check if qrcode is found on each page
-                if decode_image[0].type == 'QRCODE' or decode_image[0].type == 'BARCODE':
-                    #create new pdf and add current file
-                    pagenum = regex.findall(basename(image))[0]
-                    found  = True
-                    print('found: ' + str(pagenum))
-                    output.addPage(input_pdf.getPage(int(pagenum)))
-                    with open('splitpdf' + str(pagenum) + '.pdf', 'wb') as outputStream:
-                        output.write(outputStream)
-            else: #if no barcode is found, add current page to previous created pdf
-                pagenum = int(pagenum)
-                if found:
-                    pagenum += 1
-                    output.addPage(input_pdf.getPage(pagenum))
+        if decode_image: #check if qrcode is found on each page
+            if decode_image[0].type != "":
+                if output:
                     found  = False
-                    print("none: " + basename(image))
-                else: 
-                    output.addPage(input_pdf.getPage(pagenum))
-                    pagenum += 1
+                    with open(output_folder + year + month + day + '_' + str(filename).replace('.pdf', '') + str(pdfnum) + '.pdf', 'wb') as output_stream:
+                        output.write(output_stream)
+                        pdfnum += 1
+                output = PdfFileWriter()
+                #create new pdf and add current file
+                pagenum = regex.findall(basename(image))[0]
+                found  = True 
+                output.addPage(input_pdf.getPage(int(pagenum)))
+        else: #if no barcode is found, add current page to previous created pdf                
+            pagenum = int(pagenum)
+            if found:
+                pagenum += 1
+                output.addPage(input_pdf.getPage(pagenum))
+            else: 
+                output.addPage(input_pdf.getPage(pagenum))
+                pagenum += 1
+    if output:
+        with open(output_folder + year + month + day + '_' + str(filename).replace('.pdf', '') + str(pdfnum) + '.pdf', 'wb') as output_stream:
+            output.write(output_stream)
+            pdfnum += 1
  
     for f in os.listdir(image_folder):
         os.remove(os.path.join(image_folder, f))
     
+    if os.path.exists('img'):
+        # checking whether the folder is empty or not
+        if len(os.listdir('img')) == 0:
+            # removing the file using the os.remove() method
+            os.rmdir('img')
+    return True
+
+async def move_old_pdf(): 
+    for pdf_file in glob.glob(join(batch_folder, '*.pdf')):
+        shutil.move(pdf_file, archive_folder)
+    
+    
+def main():
+    try:
+        # Create target Directory
+        os.mkdir('input')
+        os.mkdir('archiv')
+    except FileExistsError:
+        pass
+
+    while len(os.listdir(batch_folder)) == 0:
+        pass
+    else:
+        asyncio.run(create_page_image())
+        asyncio.run(move_old_pdf())
+        main()
         
 if __name__ == '__main__':
-    create_page_image()
-    
+    main()
     
