d389bf917aceb0310f734089b4bfdb86bb37b933
= <=>
2021-08-26 23:39:09 +0200

    20210826: V1.0 Program is working

--- app.py
+++ app.py
@@ -17,28 +17,31 @@ from re import compile
 shutup.please()
 
 
-#declaration
+# global declaration
 batch_folder = './input/'
 image_folder = './img/'
 output_folder = './output/'
 archive_folder = './archiv/'
 
-#body
 async def create_page_image():
+    """
+    Convert pages to images and decode them, create new pdfs based on barcodes
+    """
     try:
         # Create target Directory
         os.mkdir('img')
         os.mkdir('output')
     except FileExistsError:
         pass
-    
+    # loop over every pdf in folder
     for pdf_file in glob.glob(join(batch_folder, '*.pdf')):
         filename = basename(pdf_file)
         print('PDF found, processing: ' + filename)
+        # create pdf reader
         input_pdf = PdfFileReader(batch_folder + filename)
         pagenum = 0
         pages = convert_from_path(batch_folder + filename, 500)
-        
+        # save pages as png
         for page in pages: 
             pagename = image_folder + filename.replace('.pdf', '') + '_' + str(pagenum) + '.png'
             pagenum += 1
@@ -53,7 +56,10 @@ async def check_code(input_pdf, filename):
     pagenum = 0
     pdfnum = 0
     output = ""
+    # get name of pdf
     pdf_name = str(basename(filename)).replace('.pdf', '')
+    
+    # get current date(day, month, year)
     today = date.today()
     current_date = today.strftime("%d/%m/%Y")
     current_date = current_date.split('/')
@@ -61,9 +67,11 @@ async def check_code(input_pdf, filename):
     month = current_date[1]
     year = current_date[2]
 
+    # decode images and create new pdfs
     for image in glob.glob(join(image_folder, '*.png')):
         read_image = cv2.imread(image)
         regex = compile(r'%s_(\d+)\.png' % (pdf_name))
+        # ignore warnings
         warnings.filterwarnings('ignore')
         decode_image = decode(read_image)
         if decode_image: #check if qrcode is found on each page
@@ -91,9 +99,11 @@ async def check_code(input_pdf, filename):
             output.write(output_stream)
             pdfnum += 1
  
+    # remove images
     for f in os.listdir(image_folder):
         os.remove(os.path.join(image_folder, f))
     
+    #remove image folder
     if os.path.exists('img'):
         # checking whether the folder is empty or not
         if len(os.listdir('img')) == 0:
@@ -102,6 +112,7 @@ async def check_code(input_pdf, filename):
     return True
 
 async def move_old_pdf(): 
+    """move processed pdf to archive"""
     for pdf_file in glob.glob(join(batch_folder, '*.pdf')):
         shutil.move(pdf_file, archive_folder)
     
