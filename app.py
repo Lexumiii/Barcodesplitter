fdda57f77146d1b4cf68caf4b15a12e66746f098
= <=>
2021-08-29 01:17:46 +0200

    20210829 : not stable

--- app.py
+++ app.py
@@ -1,54 +1,63 @@
-import shutil
 import glob
 import asyncio
 import os
 import cv2
-import shutup
 import warnings
 from os.path import join, basename
 from pdf2image import convert_from_path
 from pyzbar.pyzbar import decode
 from PyPDF2 import PdfFileWriter, PdfFileReader
-from datetime import date
 from re import compile
+from utility import ColoredPrint, CalcTime, DirHandler
+import shutup
 
 
 # supress warnings
-shutup.please()
 
+# install colored logging
+log = ColoredPrint()
 
 # global declaration
 batch_folder = './input/'
 image_folder = './img/'
 output_folder = './output/'
 archive_folder = './archiv/'
+mimetype_pdf = '.pdf'
+mimetype_img = '.png'
+
+# get current date(day, month, year)
+time = CalcTime()
+time = time.calc_time()
+
+# dir handler
+dir = DirHandler()
+
 
 async def create_page_image():
-    """
-    Convert pages to images and decode them, create new pdfs based on barcodes
-    """
-    try:
-        # Create target Directory
-        os.mkdir('img')
-        os.mkdir('output')
-    except FileExistsError:
-        pass
+    """Convert pages to images and decode them, create new pdfs based on barcodes"""
     # loop over every pdf in folder
     for pdf_file in glob.glob(join(batch_folder, '*.pdf')):
         filename = basename(pdf_file)
-        print('PDF found, processing: ' + filename)
+        log.success('PDF found, processing: ' + filename).store()
+        shutup.mute()
+
         # create pdf reader
         input_pdf = PdfFileReader(batch_folder + filename)
         pagenum = 0
         pages = convert_from_path(batch_folder + filename, 500)
         # save pages as png
         for page in pages: 
-            pagename = image_folder + filename.replace('.pdf', '') + '_' + str(pagenum) + '.png'
+            pagename = image_folder + filename.replace(mimetype_pdf, '') + '_' + str(pagenum) + mimetype_img
             pagenum += 1
             page.save(pagename, 'PNG')
             
-        await check_code(input_pdf, filename)
-        print('Finished processing: ' + filename)
+        check = await check_code(input_pdf, filename)
+        shutup.unmute()
+        if check: 
+            log.success('Finished processing: ' + filename).store()
+        else: 
+            log.warn('There was no Barcode/Qrcode in the Pdf: ' + filename)
+            dir.move_empty_pdf(filename)
     return True
            
 
@@ -56,37 +65,42 @@ async def check_code(input_pdf, filename):
     pagenum = 0
     pdfnum = 0
     output = ""
+    code_check = False
+    found = False
+    create = True
     # get name of pdf
-    pdf_name = str(basename(filename)).replace('.pdf', '')
-    
-    # get current date(day, month, year)
-    today = date.today()
-    current_date = today.strftime("%d/%m/%Y")
-    current_date = current_date.split('/')
-    day = current_date[0]
-    month = current_date[1]
-    year = current_date[2]
-
+    pdf_name = str(basename(filename)).replace(mimetype_pdf, '')
+    #print(create_message)
     # decode images and create new pdfs
     for image in glob.glob(join(image_folder, '*.png')):
+        
+        create_message = 'Created file: ' + time['year'] + time['month'] + time['day'] + '_' + str(filename).replace(mimetype_pdf, '') + str(pdfnum) + mimetype_pdf + ' -- ' + time['year'] + '-' + time['month'] + '-' + time['day'] + ' ' + time['hour'] + ':' + time['minute'] + ':' + time['second']
         read_image = cv2.imread(image)
         regex = compile(r'%s_(\d+)\.png' % (pdf_name))
         # ignore warnings
         warnings.filterwarnings('ignore')
+
         decode_image = decode(read_image)
         if decode_image: #check if qrcode is found on each page
             if decode_image[0].type != "":
+                code_check = True
+                create = False
                 if output:
                     found  = False
-                    with open(output_folder + year + month + day + '_' + str(filename).replace('.pdf', '') + str(pdfnum) + '.pdf', 'wb') as output_stream:
+                    with open(output_folder + time['year'] + time['month'] + time['day'] + '_' + str(filename).replace(mimetype_pdf, '') + str(pdfnum) + mimetype_pdf, 'wb') as output_stream:
                         output.write(output_stream)
+                        log.write(create_message).store()
                         pdfnum += 1
                 output = PdfFileWriter()
                 #create new pdf and add current file
                 pagenum = regex.findall(basename(image))[0]
                 found  = True 
                 output.addPage(input_pdf.getPage(int(pagenum)))
-        else: #if no barcode is found, add current page to previous created pdf                
+        else: #if no barcode is found, add current page to previous created pdf       
+            if create:
+                # remove images
+                dir.remove_images()
+                return code_check    
             pagenum = int(pagenum)
             if found:
                 pagenum += 1
@@ -95,41 +109,30 @@ async def check_code(input_pdf, filename):
                 output.addPage(input_pdf.getPage(pagenum))
                 pagenum += 1
     if output:
-        with open(output_folder + year + month + day + '_' + str(filename).replace('.pdf', '') + str(pdfnum) + '.pdf', 'wb') as output_stream:
+        with open(output_folder + time['year'] + time['month'] + time['day'] + '_' + str(filename).replace(mimetype_pdf, '') + str(pdfnum) + mimetype_pdf, 'wb') as output_stream:
             output.write(output_stream)
+            log.write(create_message).store()
             pdfnum += 1
  
     # remove images
-    for f in os.listdir(image_folder):
-        os.remove(os.path.join(image_folder, f))
+    dir.remove_images()
     
-    #remove image folder
-    if os.path.exists('img'):
-        # checking whether the folder is empty or not
-        if len(os.listdir('img')) == 0:
-            # removing the file using the os.remove() method
-            os.rmdir('img')
-    return True
+    # remove image folder
+    dir.remove_folder('img')
+    return code_check
 
-async def move_old_pdf(): 
-    """move processed pdf to archive"""
-    for pdf_file in glob.glob(join(batch_folder, '*.pdf')):
-        shutil.move(pdf_file, archive_folder)
-    
-    
+
 def main():
-    try:
-        # Create target Directory
-        os.mkdir('input')
-        os.mkdir('archiv')
-    except FileExistsError:
-        pass
-
+    # Create target Directory
+    dir.make_dir('input')
+    dir.make_dir('archiv')
+    dir.make_dir('img')
+    dir.make_dir('output')
     while len(os.listdir(batch_folder)) == 0:
         pass
     else:
         asyncio.run(create_page_image())
-        asyncio.run(move_old_pdf())
+        dir.move_old_pdf()
         main()
         
 if __name__ == '__main__':
---
+++ utility.py
@@ -0,0 +1,125 @@
+
+from datetime import datetime, date
+import glob
+from os.path import join, basename
+import os
+import shutil
+
+class ColoredPrint:
+    def __init__(self):
+        self.PINK = '\033[95m'
+        self.OKBLUE = '\033[94m'
+        self.OKGREEN = '\033[92m'
+        self.WARNING = '\033[93m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+        self.LOGF = 'logfile.log'
+        self.msg = ''
+
+    def disable(self):
+        self.PINK = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
+    def store(self):
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open(self.LOGF, mode='a') as file_:
+            file_.write(f"{self.msg} -- {date}")
+            file_.write("\n")
+
+    def success(self, *args, **kwargs):
+        self.msg = ' '.join(map(str, args))
+        print(self.OKGREEN + self.msg + self.ENDC, **kwargs)
+        return self
+
+    def info(self, *args, **kwargs):
+        self.msg = ' '.join(map(str, args))
+        print(self.OKBLUE + self.msg + self.ENDC, **kwargs)
+        return self
+
+    def warn(self, *args, **kwargs):
+        self.msg = ' '.join(map(str, args))
+        print(self.WARNING + self.msg + self.ENDC, **kwargs)
+        return self
+
+    def err(self, *args, **kwargs):
+        self.msg = ' '.join(map(str, args))
+        print(self.FAIL + self.msg + self.ENDC, **kwargs)
+        return self
+
+    def pink(self, *args, **kwargs):
+        self.msg = ' '.join(map(str, args))
+        print(self.PINK + self.msg + self.ENDC, **kwargs)
+        return self
+    
+    def write(self, *args):
+        self.msg = ' '.join(map(str, args))
+        return self
+    
+    def get_line(self, lookup):
+        with open(self.LOGF) as myFile:
+            for num, line in enumerate(myFile, 1):
+                if str(lookup) in line:
+                    print('found at line:', num)
+        return self
+class CalcTime:
+    def __init__(self):
+        self.msg = 'Create date'
+        
+    def calc_time(self):
+        today = datetime.now()
+        d = dict()
+        current_date = today.strftime("%d/%m/%Y/%H/%M/%S")
+        current_date = current_date.split('/')
+        d['day'] = current_date[0]
+        d['month'] = current_date[1]
+        d['year'] = current_date[2]
+        d['hour'] = current_date[3]
+        d['minute'] = current_date[4]
+        d['second'] = current_date[5]
+        return d
+    
+class DirHandler:
+    def __init__(self):
+        self.image_folder = './img/'
+        self.batch_folder = './input/'
+        self.output_folder = './output/'
+        self.archive_folder = './archiv/'
+        self.mimetype_pdf = 'pdf'
+        self.mimetype_img = 'img'
+
+        
+    def make_dir(self, dir_name):
+        try:
+            os.mkdir(dir_name)
+        except FileExistsError:
+            pass
+        
+    def remove_images(self):
+        # remove images
+        for f in os.listdir(self.image_folder):
+            os.remove(os.path.join(self.image_folder, f))
+
+    def remove_folder(self, folder):
+        #remove image folder
+        if os.path.exists(folder):
+            # checking whether the folder is empty or not
+            if len(os.listdir(folder)) == 0:
+                # removing the file using the os.remove() method
+                os.rmdir(folder)
+
+    def move_old_pdf(self): 
+        """move processed pdf to archive"""
+        for pdf_file in glob.glob(join(self.batch_folder, '*.pdf')):
+            shutil.move(pdf_file, self.archive_folder)
+            
+    def move_empty_pdf(self, filename):
+        log = ColoredPrint()
+        log.info('Move file %s with no barcode/qrcode to output folder' % (filename)).store()
+        shutil.move(self.batch_folder + filename, self.output_folder)
+        log.info('Finished moving %s to output folder' % (filename)).store()
+        
+        
